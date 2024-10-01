2bf496b3ece98ea5bef22c26a6f2fed85c1ba4f2
김예환 Ye-Hwan Kim (Sam) <yehwankim23@gmail.com>
2024-10-01 14:27:57 +0900

    Update data

--- src/themes.js
+++ src/themes.js
@@ -1,17 +1,17 @@
 const themes = {
-  light: { backgroundColor: "#ffffff", languageColor: "#1f2328", percentColor: "#636c76" },
+  light: { backgroundColor: "#ffffff", languageColor: "#1f2328", percentColor: "#59636e" },
   light_high_contrast: {
     backgroundColor: "#ffffff",
     languageColor: "#010409",
-    percentColor: "#0e1116",
+    percentColor: "#454c54",
   },
-  dark: { backgroundColor: "#0d1117", languageColor: "#e6edf3", percentColor: "#8d96a0" },
+  dark: { backgroundColor: "#0d1117", languageColor: "#f0f6fc", percentColor: "#9198a1" },
   dark_high_contrast: {
-    backgroundColor: "#0a0c10",
-    languageColor: "#fbfcfd",
-    percentColor: "#f0f3f6",
+    backgroundColor: "#010409",
+    languageColor: "#ffffff",
+    percentColor: "#b7bdc8",
   },
-  dark_dimmed: { backgroundColor: "#22272e", languageColor: "#c5d1de", percentColor: "#778491" },
+  dark_dimmed: { backgroundColor: "#212830", languageColor: "#d1d7e0", percentColor: "#9198a1" },
 };
 
 export { themes };
